423be749297da87b4f94c2ea372e681b2e1c1385
walageri <gerygilyia@gmail.com>
2017-12-10 14:19:13 +0700

    v1

---
+++ app/src/main/java/com/blogspot/progaramer27android/timerapp/Main3Activity.java
@@ -0,0 +1,22 @@
+package com.blogspot.progaramer27android.timerapp;
+
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+
+    @Override
+    public void onBackPressed() {
+        moveTaskToBack(true);
+        android.os.Process.killProcess(android.os.Process.myPid());
+        System.exit(0);
+        finish();
+    }
+}
---
+++ app/src/main/java/com/blogspot/progaramer27android/timerapp/MainActivity.java
@@ -0,0 +1,51 @@
+package com.blogspot.progaramer27android.timerapp;
+
+import android.content.Intent;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import java.util.Timer;
+
+public class MainActivity extends AppCompatActivity {
+    TextView show;
+    Timer timer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        show=(TextView)findViewById(R.id.text);
+        timer= new Timer(120000,1000) ;
+
+        show.setText("10");
+        String text = show.getText().toString();
+        if (text.equalsIgnoreCase("selsai")){
+
+        }
+    }
+    public void start(View view){
+        timer.start();
+    }
+    public void stop(View view){
+        timer.cancel();
+    }
+    public void next(View view){
+        startActivity(new Intent(this,Main2Activity.class));
+    }
+    public class Timer extends CountDownTimer {
+
+        public Timer( long millisInFuture, long countDownInterval){
+            super(millisInFuture, countDownInterval);
+        }
+            @Override
+            public void onTick ( long l){
+            show.setText("" + (l / 60000));
+        }
+            @Override
+            public void onFinish () {
+            show.setText("selsai");
+                startActivity(new Intent(MainActivity.this,Main2Activity.class));
+        }
+    }
+}
